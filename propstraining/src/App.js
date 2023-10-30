81b2d031bb9da025d4a5c1f49ca47b711e4e7c5a
Yevhen Larin <minard1usz@gmail.com>
2023-10-30 15:31:35 +0200

    new

--- propstraining/src/App.js
+++ propstraining/src/App.js
@@ -1,12 +1,31 @@
+import { Component } from 'react';
 import './App.css';
 
-function WhoAmI ({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        age: 27,
+        text: '+++'
+      }
+    }
+
+    nextYear = () => {
+      this.setState(state => ({
+          age: state.age + 1
+        }))
+    }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.age}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 
