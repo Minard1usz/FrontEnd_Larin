75c054e2ffd1a54014028366a8efe4f43f22c055
Yevhen Larin <minard1usz@gmail.com>
2023-11-03 15:19:59 +0200

    immutability

--- myreactproject/src/components/app/App.js
+++ myreactproject/src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,28 +6,62 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 8500, increase: false, id: 1},
+        {name: 'Alex L.', salary: 4800, increase: true, id: 2},
+        {name: 'Carl W.', salary: 9100, increase: false, id: 3}
+      ]
+    }
+    this.maxId = 4;
+  }
 
-  const data = [
-    {name: 'John C.', salary: 8500, increase: false, id: 1},
-    {name: 'Alex L.', salary: 4800, increase: true, id: 2},
-    {name: 'Carl W.', salary: 9100, increase: false, id: 3}
-  ];
+  addItem = (name, salary) => {
+      const newItem = {
+        name,
+        salary,
+        increase: false,
+        id: this.maxId++
+      }
+      this.setState(({data}) => {
+        const newArr = [...data, newItem];
+        return {
+          data: newArr
+        }
+      })
+    }
 
-  return (
-   <div className="app">
-    <AppInfo />
+    deleteItem = (id) => {
+      this.setState(({data}) => {
 
-    <div className="search-panel">
-      <SearchPanel />
-      <AppFilter />
+        return {
+          data: data.filter(item => item.id !== id)
+        }
+      })
+  }
+
+  render() {
+      return (
+        <div className="app">
+          <AppInfo />
       
-    </div>
+          <div className="search-panel">
+            <SearchPanel />
+            <AppFilter />
+            
+          </div>
+          <EmployeesList 
+          data={this.state.data} 
+          onDelete={this.deleteItem}/>
 
-    <EmployeesList data={data} />
-    <EmployeesAddForm />
-   </div>
-  );
+          <EmployeesAddForm onAdd={this.addItem}/>
+        
+        </div>
+      );
+    }
 }
 
 export default App;
--- myreactproject/src/components/employees-list-item/employees-list-item.js
+++ myreactproject/src/components/employees-list-item/employees-list-item.js
@@ -23,9 +23,8 @@ class EmployeesListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
-        const {increase} = this.state;
-        const {favorites} = this.state;
+        const {name, salary, onDelete} = this.props;
+        const {increase, favorites} = this.state;
 
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
@@ -36,25 +35,25 @@ class EmployeesListItem extends Component {
         }
 
     return (
-        <li className={classNames}>
-            <span className="list-group-item-label"
-            onClick={this.inFavorites}>{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm "
-                    onClick={this.onIncrease}>
-                    <i className="fas fa-cookie"></i>
-                </button>
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.inFavorites}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
     }
 }
 
