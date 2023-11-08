7f3c402f7e9b3bfd780aebdfdba42986fa157ae2
Yevhen Larin <minard1usz@gmail.com>
2023-11-08 20:51:17 +0200

    third practice

--- script.js
+++ script.js
@@ -80,3 +80,61 @@ function fifthTask() {
     console.log(arr);
 }
 fifthTask();
+// ----------------------------------------------------
+
+/*
+Задачи:
+
+6) Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+Должен получиться точно такой же массив */
+
+function sixthTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    result.push(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+
+    console.log(result);
+    return result;
+}
+sixthTask();
+
+
+/*
+7) Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+Для определения типа данных используйте typeof();
+Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ] */
+function seventhTask() {
+    const data = [10, 20, 'Shopping', 20, 'Homework'];
+
+    for (i = 0; i < data.length; i++) {
+        if (typeof data[i] === 'number') {
+            data[i] *= 2;
+        } else if (typeof data[i] === 'string') {
+            data[i] += ' - done';
+        }
+    } 
+    console.log(data);
+    return data;
+}
+seventhTask();
+
+
+/*
+8) Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+*/
+function eightTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 0; i < data.length; i++) {
+        result.push(data[i])
+    }
+    result.reverse()
+    console.log(result);
+    return result;
+}
+eightTask();
+
+
