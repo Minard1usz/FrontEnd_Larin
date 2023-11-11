415bbc04268665c93a9267f41093fee8f98aaa3e
Yevhen Larin <minard1usz@gmail.com>
2023-11-11 22:53:45 +0200

    evening practice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
 const personalMovieDB = {
@@ -9,13 +9,32 @@ const personalMovieDB = {
     private: false
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+function askUser() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+            
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно много фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман!');
+        } else {
+            console.log('Произошла ошибка');
+        }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+}
+askUser();
 
+// переписать цикл еще двумя способами
 
 console.log(personalMovieDB);
