85b8ffefcc0fba0998be7c2b8e0ff019ea764531
Yevhen Larin <minard1usz@gmail.com>
2023-11-29 22:54:14 +0200

    explanation

--- script.js
+++ script.js
@@ -1,130 +1,61 @@
-let numberOfFilms;
+// Callback функция - это функция, которая передана в качестве аргумента в другой функции.
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-}
-
-function writeYourGenres() {
-    let i = 1;
-    while (i <= 3){
-        const favoriteGenres = prompt(`Ваш любимый жанр под номером ${i}`).trim();
-        personalMovieDB.genres.push(favoriteGenres);
-        i++;
-    }
-}
-writeYourGenres();
-
-function askUser() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
-              b = prompt('На сколько оцените его?', '');
-
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-}
-askUser();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно много фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('Вы киноман!');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
-detectPersonalLevel();
-
-function showMyDB() {
-    if (personalMovieDB.private == false) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
-//-----------------------------------------------------------
-// переписать цикл еще двумя способами
-// 2) цикл While()
-// function askMe() {
-//     let i = 0;
-//     while (i < 2) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//         i++;
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//         }
-//     }
-    // if (personalMovieDB.count < 10) {
-    //     console.log('Просмотрено довольно много фильмов');
-    // } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    //     console.log('Вы классический зритель');
-    // } else if (personalMovieDB.count >= 30) {
-    //     console.log('Вы киноман!');
-    // } else {
-    //     console.log('Произошла ошибка');
-    // }
-    
+// Объяснение с Udemy Иван Петриченко
+// function first() {
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
 // }
-// askMe();
-//-----------------------------------------------------------
-// 3) do...while
-// function askMore() {
-//     let i = 0;
-//     do {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//         i++;
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             console.log('error');
-//         }
-//     } while (i < 2);
-//     if (personalMovieDB.count < 10) {
-//         console.log('Просмотрено довольно много фильмов');
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//         console.log('Вы классический зритель');
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log('Вы киноман!');
-//     } else {
-//         console.log('Произошла ошибка');
-//     }
+
+// function second() {
+//     console.log(2);
 // }
-// askMore();
-//-----------------------------------------------------------
-/*  isNaN() - проверяет сущность на "не число", если данные "не число" - true
-*/
-//
 
 
-// console.log(personalMovieDB);
+// first();
+// second();
 
 
-//string.trim() - Метод trim() удаляет пробельные символы с начала и конца строки. 
-//Пробельными символами в этом контексте считаются все собственно пробельные символы (пробел, табуляция, неразрывный пробел и прочие) 
-//и все символы конца строки (LF, CR и прочие).
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+//---------------------------------------------------------------
+
+// Объяснение ItGid.info с YouTube
+// function first(argumentFunction) {
+//     console.log(1);
+//     argumentFunction();
+// }
+
+// function second(a, b) {
+//     console.log(a + b);
+// }
+
+// first(function() {
+//     second(5, 7);
+// });
+// second(4, 7);
+
+// необходима функция-обёртка
+
+//---------------------------------------------------------------
+//Мой пример
+function firstFunc(elem) {
+    console.log('Привет! Сейчас запущу вторую функцию...');
+    setTimeout(function () {
+        elem();
+    }, 500);
+}
+
+function secondFunc() {
+    console.log('Готово!');
+}
+
+firstFunc(secondFunc);
