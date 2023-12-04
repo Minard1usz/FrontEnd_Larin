1eb40e7ebb0b76d6391d1a72aad8b920299a76dc
Yevhen Larin <minard1usz@gmail.com>
2023-12-04 13:30:53 +0200

    new practice

--- script.js
+++ script.js
@@ -110,9 +110,112 @@ obj3.name = 'David';
 console.log(obj3);
 
 // Методы объектов (действия, функции)
-let obj4 = {
-    sayName: function() {
-        return alert('David')
+// let obj4 = {
+//     sayName: function() {
+//         return alert('David')
+//     }
+// }
+// console.log(obj4.sayName());
+
+
+
+//------------------------------------------------------------------------------
+// Задания с Udemy
+/* 1) 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+ */
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+const family2 = [];
+
+function showFamily(arr) {
+    let str = '';
+    if (Object.keys(arr).length !== 0) {
+        str += `Семья состоит из: ${arr.join(', ')}`
+        console.log(str);
+    } else {
+        str += `Семья пуста`
+        console.log(str);
     }
 }
-console.log(obj4.sayName());
+showFamily(family);
+showFamily(family2);
+//-----------------------------------------------------
+/* 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+Пример:
+standardizeStrings(favoriteCities)  выведет в консоль
+lisbon
+rome
+milan
+dublin
+Это частая задача в реальности, так как от пользователя нам могут прийти ответы в самых разных форматах. 
+В том числе и с разными буквами :) Поэтому нам нужно привести строки в один формат для правильной работы.
+*/
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    let str = '';
+    str += arr.join('\n');
+    return console.log(str.toLowerCase());
+}
+standardizeStrings(favoriteCities);
+//-------------------------------------------------------------
+/* 3) Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+Пример:
+const someString = 'This is some strange string';
+reverse(someString) => 'gnirts egnarts emos si sihT'
+Функцию можно применить к любой строке. 
+Если в функцию приходит не строка - вернуть сообщение "Ошибка!"
+Это очень интересная задача, которую можно решить несколькими способами. 
+Её дают для того, чтобы оценить навыки и знания программиста, посмотреть как он думает. 
+*/
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    let data = str;
+    if (typeof(data) !== 'string') {
+        console.log('error');
+    }
+    const arr = data.split('');
+    arr.reverse();
+    return console.log(arr.join(''));
+}
+reverse(someString);
+
+//---------------------------------------------
+/* 4) Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+Пример:
+availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+Вернет строку:
+Доступные валюты:
+UAH
+RUB
+Заметьте:
+- CNY (юань) исчез из списка валют, значит такая валюта закончилась
+- После валюты: стоит перенос строки \n, и после каждой валюты тоже. Это важно для тестов
+- Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку
+*/
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    arr = [...baseCurrencies, ...additionalCurrencies];
+    if (Object.keys(arr).length === 0) {
+        missingCurr = 'Нет доступных валют';
+        console.log(missingCurr);
+    } else {
+        console.log(`Доступные валюты:\n`)
+        arr.forEach(function(curr, i){
+            if (curr !== missingCurr) {
+                console.log(`${curr}\n`)
+            }
+        })
+    }
+    return;
+}
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
+
+// Массивы можно объединить с помощью (...) - spread оператора.
